a0a68888462de5df4efd2e0e2ef78409de032978
ma1ex <matvienko.alexey@gmail.com>
2019-08-08 23:32:23 +0300

    Updates

---
+++ precache-manifest.dcc205770a86a4984d00dced2820a8de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5baaafd4e758426d983ebcd3043e3da4",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "9d3fc12925f0017ceedb",
+    "url": "/star-db/static/css/main.949dc0b3.chunk.css"
+  },
+  {
+    "revision": "59df7a466a2de19381c8",
+    "url": "/star-db/static/js/2.a6d9c573.chunk.js"
+  },
+  {
+    "revision": "9d3fc12925f0017ceedb",
+    "url": "/star-db/static/js/main.776f623f.chunk.js"
+  },
+  {
+    "revision": "7fd0c1ce602d36977d9a",
+    "url": "/star-db/static/js/runtime~main.82ef0156.js"
+  }
+]);
