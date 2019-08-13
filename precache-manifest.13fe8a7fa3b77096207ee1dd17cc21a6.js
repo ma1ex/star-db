ebf6586e6337695826525dcccb17dfb7028ba7c0
ma1ex <matvienko.alexey@gmail.com>
2019-08-14 01:40:17 +0300

    Updates

---
+++ precache-manifest.13fe8a7fa3b77096207ee1dd17cc21a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961249adcc664d66c52347e024f984e2",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "1e9aa8f44ce54ed37888",
+    "url": "/star-db/static/css/main.4c194f8a.chunk.css"
+  },
+  {
+    "revision": "28f74768b0c8c73e65f9",
+    "url": "/star-db/static/js/2.64605880.chunk.js"
+  },
+  {
+    "revision": "1e9aa8f44ce54ed37888",
+    "url": "/star-db/static/js/main.9f72d2ab.chunk.js"
+  },
+  {
+    "revision": "7fd0c1ce602d36977d9a",
+    "url": "/star-db/static/js/runtime~main.82ef0156.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/star-db/precache-manifest.1ad65b432294c041b980006c5196e8d8.js"
+  "/star-db/precache-manifest.13fe8a7fa3b77096207ee1dd17cc21a6.js"
 );
 
 self.addEventListener('message', (event) => {
