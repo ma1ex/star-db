4b2168136fb3cf96ecc2c382992da5d11ae909c7
ma1ex <matvienko.alexey@gmail.com>
2019-08-22 23:33:56 +0300

    Refactor

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -7,26 +7,26 @@ const withData = (View, getData) => {
   return class extends Component {
 
     state = {
-      data: null
+        data: null
     };
 
     componentDidMount() {
-      getData()
-        .then((data) => {
-          this.setState({
-            data
-          });
-        });
+        getData()
+            .then((data) => {
+                this.setState({
+                    data
+                });
+            });
     }
 
     render() {
-      const { data } = this.state;
+        const { data } = this.state;
 
-      if (!data) {
-        return <Spinner />;
-      }
+        if (!data) {
+            return <Spinner />;
+        }
 
-      return <View {...this.props} data={data} />;
+        return <View {...this.props} data={data} />;
     }
   };
 };
