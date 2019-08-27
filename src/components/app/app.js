0e82a89bb741b1b0ac7254e1b123b7bd5f8a9699
ma1ex <matvienko.alexey@gmail.com>
2019-08-27 23:54:50 +0300

    Refactoring. Added React Context in swapi-service-context component to replace SwapiService

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,10 @@ import ErrorBoundry from '../error-boundry';
 import ErrorButton from '../error-button';
 import PeoplePage from '../people-page';
 import Row from '../row';
+
+import SwapiService from '../../services/swapi-service';
+import { SwapiServiceProvider } from '../swapi-service-context';
+
 import {
     PlanetDetails,
     StarshipDetails,
@@ -19,29 +23,33 @@ import './app.css';
 
 export default class App extends Component {
     
+    swapiService = new SwapiService();
+
     render() {
         
         return (
             <ErrorBoundry>
-                <div className="row">
-                    <Header />
-                    <RandomPlanet />
+                <SwapiServiceProvider value={this.swapiService}>
+                    <div className="row">
+                        <Header />
+                        <RandomPlanet />
 
-                    <div className="container button-row">
-                        <ErrorButton />
+                        <div className="container button-row">
+                            <ErrorButton />
+                        </div>
+
+                        <PeoplePage />
+
+                        <Row left={ <StarshipList /> } right={
+                            <StarshipDetails itemId={9} />
+                        } />
+
+                        <Row left={ <PlanetList /> } right={
+                            <PlanetDetails itemId={4} />
+                        } />
+                        
                     </div>
-
-                    <PeoplePage />
-
-                    <Row left={ <StarshipList /> } right={
-                        <StarshipDetails itemId={9} />
-                    } />
-
-                    <Row left={ <PlanetList /> } right={
-                        <PlanetDetails itemId={4} />
-                    } />
-                    
-                </div>
+                </SwapiServiceProvider>
             </ErrorBoundry>
         )
 
--- src/components/sw-components/details.js
+++ src/components/sw-components/details.js
@@ -1,57 +1,82 @@
 import React from 'react';
 
 import ItemDetails, { Record } from '../item-details/item-details';
-import SwapiService from '../../services/swapi-service';
+// import SwapiService from '../../services/swapi-service';
+import { SwapiServiceConsumer } from '../swapi-service-context';
 
-const swapiService = new SwapiService();
+// const swapiService = new SwapiService();
 
-const {
+/* const {
     getPerson,
     getPlanet,
     getStarship,
     getPersoneImage,
     getPlanetImage,
     getStarshipImage
-} = swapiService;
+} = swapiService; */
 
 
 const PersonDetails = ({ itemId }) => {
     return (
-        <ItemDetails 
-            itemId={itemId} 
-            getData={ getPerson } 
-            getImageUrl={getPersoneImage}>
-                <Record field="gender" label="Gender" />
-                <Record field="eyeColor" label="Eye Color" />
-                <Record field="birthYear" label="Birth Year" />
-        </ItemDetails>
+        <SwapiServiceConsumer>
+            {
+                ({ getPerson, getPersoneImage }) => {
+                    return (
+                        <ItemDetails 
+                            itemId={itemId} 
+                            getData={ getPerson } 
+                            getImageUrl={getPersoneImage}>
+                                <Record field="gender" label="Gender" />
+                                <Record field="eyeColor" label="Eye Color" />
+                                <Record field="birthYear" label="Birth Year" />
+                        </ItemDetails>
+                    );
+                }
+            }
+        </SwapiServiceConsumer>
     );
 };
 
 const PlanetDetails = ({ itemId }) => {
     return (
-        <ItemDetails 
-            itemId={itemId}
-            getData={ getPlanet } 
-            getImageUrl={getPlanetImage}>
-                <Record field="name" label="Name" />
-                <Record field="population" label="Population" />
-                <Record field="rotationPeriod" label="Rotation perhiod" />
-                <Record field="diameter" label="Diameter" />
-        </ItemDetails>
+        <SwapiServiceConsumer>
+            {
+                ({ getPlanet, getPlanetImage }) => {
+                    return (
+                        <ItemDetails 
+                            itemId={itemId}
+                            getData={ getPlanet } 
+                            getImageUrl={getPlanetImage}>
+                                <Record field="name" label="Name" />
+                                <Record field="population" label="Population" />
+                                <Record field="rotationPeriod" label="Rotation perhiod" />
+                                <Record field="diameter" label="Diameter" />
+                        </ItemDetails>
+                    );
+                }
+            }
+        </SwapiServiceConsumer>
     );
 };
 
 const StarshipDetails = ({ itemId }) => {
     return (
-        <ItemDetails 
-            itemId={itemId}
-            getData={ getStarship } 
-            getImageUrl={getStarshipImage}>
-                <Record field="model" label="Model" />
-                <Record field="length" label="Length" />
-                <Record field="costInCredits" label="Cost" />
-        </ItemDetails>
+        <SwapiServiceConsumer>
+            {
+                ({ getStarship, getStarshipImage }) => {
+                    return (
+                        <ItemDetails 
+                            itemId={itemId}
+                            getData={ getStarship } 
+                            getImageUrl={getStarshipImage}>
+                                <Record field="model" label="Model" />
+                                <Record field="length" label="Length" />
+                                <Record field="costInCredits" label="Cost" />
+                        </ItemDetails>
+                    );
+                }
+            }
+        </SwapiServiceConsumer>
     );
 };
 
